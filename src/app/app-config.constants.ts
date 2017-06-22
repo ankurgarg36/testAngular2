57e85c1d3aac60c26f97a4ff31b19cc583a78946
ankur <ankur.garg@misport.com>
2017-06-22 18:16:38 +0530

    Code Optimization:Load those request which are required. Removed un necessary hits.

--- src/app/app-config.constants.ts
+++ src/app/app-config.constants.ts
@@ -3,44 +3,42 @@ import {IAppConfig} from './app-config.interface';
 import {environment} from '../environments/environment';
 
 
-
-
 export const AppConfig: IAppConfig = {
-  apiEndpoint: environment.apiEndpoint
+    apiEndpoint: environment.apiEndpoint
 };
 
-export let APP_CONFIG = new InjectionToken< IAppConfig >( 'app.config' );
+export let APP_CONFIG = new InjectionToken<IAppConfig>('app.config');
 
 
 export const MenuLinks = [
-    {url: '/home' , text: 'Home'},
-    {url: '/about-us' , text: 'About Us'},
-    {url: '/sarees' , text: 'Sarees'},
-    {url: '/suit' , text: 'Suit-Salvar'},
-    {url: '/lengha' , text: 'Lengha-Chunni'},
-    {url: '/contact' , text: 'Contact Us'},
+    {url: '/home', text: 'Home'},
+    {url: '/about-us', text: 'About Us'},
+    {url: '/sarees', text: 'Sarees'},
+    {url: '/suit', text: 'Suit-Salvar'},
+    {url: '/lengha', text: 'Lengha-Chunni'},
+    {url: '/contact', text: 'Contact Us'},
 ];
 
 export const ProductType = {
-  saree: 1,
-  suit: 2,
-  lengha: 3,
+    saree: 1,
+    suit: 2,
+    lengha: 3,
 };
 
 export const ProductCategory = {
-  saree: 'saree',
-  suit: 'suit',
-  lengha: 'lengha',
+    saree: 'saree',
+    suit: 'suit',
+    lengha: 'lengha',
 };
 
 export const PriceRange = [
-  {range: '0-1000', text: 'less than 1000'},
-  {range: '1000-1500', text: '1000-1500'},
-  {range: '1500-2000', text: '1500-2000'},
-  {range: '2000-3000', text: '2000-3000'},
-  {range: '3000', text: 'Above 3000'}
+    {range: '0-1000', text: 'less than 1000'},
+    {range: '1000-1500', text: '1000-1500'},
+    {range: '1500-2000', text: '1500-2000'},
+    {range: '2000-3000', text: '2000-3000'},
+    {range: '3000', text: 'Above 3000'}
 ];
 
 export const Regex = {
-  email : /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
 };
--- src/app/modules/home/home-tab-component/home-tab.component.ts
+++ src/app/modules/home/home-tab-component/home-tab.component.ts
@@ -1,30 +1,49 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductService} from '../../../services/product.service';
+import {ProductCategory} from '../../../app-config.constants';
+import {ProductResponse} from '../../../response/product.response';
 
 @Component({
   moduleId: module.id,
   selector: 'home-tab',
   templateUrl: 'home-tab.component.html',
-   providers: [ProductService]
+  providers: [ProductService]
 })
 
 export class HomeTabComponent implements OnInit {
 
-  public pSaree;
-  public pSuit;
-  public pLengha;
-  constructor(private _productService: ProductService) {}
+  public pSaree: ProductResponse;
+  public pSuit: ProductResponse;
+  public pLengha: ProductResponse;
+
+  public productCategory: object;
+
+  constructor(private _productService: ProductService) {
+    this.productCategory = ProductCategory;
+  }
 
   ngOnInit(): void {
-    this._productService.getProducts('saree', 10, 1).then(pResponse => {
+    this._productService.getProducts(ProductCategory.saree, 10, 1).then(pResponse => {
       this.pSaree = pResponse.data;
     });
-    this._productService.getProducts('suit', 10, 1).then(pResponse => {
-      this.pSuit = pResponse.data;
-    });
-    this._productService.getProducts('lengha', 10, 1).then(pResponse => {
-      this.pLengha = pResponse.data;
-    });
+  }
+
+  public getProducts(config: any): void {
+    if (config.nextId === ProductCategory.saree) {
+      this._productService.getProducts(ProductCategory.saree, 10, 1).then(pResponse => {
+        this.pSaree = pResponse.data;
+      });
+    }
+    if (config.nextId === ProductCategory.suit) {
+      this._productService.getProducts(ProductCategory.saree, 10, 1).then(pResponse => {
+        this.pSuit = pResponse.data;
+      });
+    }
+    if (config.nextId === ProductCategory.lengha) {
+      this._productService.getProducts(ProductCategory.saree, 10, 1).then(pResponse => {
+        this.pLengha = pResponse.data;
+      });
+    }
   }
 
 }
